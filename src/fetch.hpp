2e07b531f056caa8c3c40cdd520c33d265d3c83b
David J. H. Shih <djh.shih@gmail.com>
2017-01-23 16:42:12 -0500

    Refactor.

--- src/fetch.hpp
+++ src/fetch.hpp
@@ -463,32 +463,3 @@ struct fetcher {
 	}
 };
 
-int main(int argc, char** argv) {
-	char path[] = "../data/KP7-092916.bam"; 
-
-	fetcher f;
-
-	f.open(path);
-
-	// set target region
-	char target[] = "chr17";
-	int32_t rid = bam_name2id(f.hdr, target);
-	//int pos = 7674420 - 1;  // 8 A, 94 G
-	int32_t pos = 7674361 - 1;   // 1 A, 2 C, 0 G, 536 T, 202 del, 15 ins
-	//int pos = 7674360 - 1;    // 0 A, 759 C, 0 G, 9 T
-	
-	//f.fetch(rid, pos);
-	f.fetch(rid, pos, nuc_C, true);
-
-	for (size_t i = 0; i < f.pile.queries.size(); ++i) {
-		bam1_t *b = f.pile.queries[i];
-		print_query_fastq(b);
-	}	
-	cout << endl;
-	for (size_t i = 0; i < f.pile.mates.size(); ++i) {
-		bam1_t *b = f.pile.mates[i];
-		print_query_fastq(b);
-	}	
-
-	return 0;
-}
