b4c7fa7e6f0bfe0e248199868bb2f02a3601b1e1
David J. H. Shih <djh.shih@gmail.com>
2017-01-30 11:17:46 -0500

    Add mlat-filter test.

--- src/test-mlat-filter.sh
+++ src/test-mlat-filter.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-./hts-mlat-filter snvs.tsv ../data/KP7-092916.bam ../data/TP53_hg38_rc.2bit mlat.tsv 7668402 1 0
+mkdir -p out/mlat-filter
 
+./hts-mlat-filter test/mlat-filter/snvs.tsv ../data/test.bam ../data/TP53_hg38_rc.2bit \
+	out/mlat-filter/mlat.tsv 7668402 1 0
+
+echo "Testing diff ...";
+
+diff out/mlat-filter/mlat.tsv test/mlat-filter/mlat.tsv
