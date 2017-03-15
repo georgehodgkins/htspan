dc3ea072dec8c0c312ab86e07048ae8254c0e1f8
David J. H. Shih <djh.shih@gmail.com>
2017-03-15 15:05:49 -0400

    Added hts-count.

---
+++ src/hts-count.cpp
@@ -0,0 +1,207 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <queue>
+#include <vector>
+#include <cassert>
+#include <cstdlib>
+using namespace std;
+
+#include <htslib/hts.h>
+#include <htslib/sam.h>
+
+#include "fetcher.hpp"
+
+
+void read_snv_record(const string& line, const fetcher& f, int32_t& rid, 
+		int32_t& pos, nuc_t& ref, nuc_t& alt) {
+	
+	istringstream stream(line);
+
+	string chrom, pos_str;
+	char ref_c, alt_c;
+	stream >> chrom >> pos_str >> ref_c >> alt_c;
+
+	rid = bam_name2id(f.hdr, chrom.c_str());
+	if (rid == -1) {
+		// reference name not found: try adding chr prefix
+		string chrom2 = "chr";
+		chrom2 += chrom;
+		rid = bam_name2id(f.hdr, chrom2.c_str());
+	}
+
+	pos = atol(pos_str.c_str()) - 1;
+	ref = char_to_nuc(ref_c);
+	alt = char_to_nuc(alt_c);
+}
+
+
+int main(int argc, char** argv) {
+	--argc;
+
+	if (argc != 6) {
+		cerr << "usage: " << argv[0]
+			<< " <snv.tsv> <reads.bam> <cov.tsv> <keep_dup> <min_mapq> <min_baseq>" << endl << endl;
+		cerr << "Format specification of the SNV file: " << endl;
+		cerr << "snvfile ::=  header [TAB record]+" << endl;
+		cerr << "header  ::=  'chrom' TAB 'pos' TAB 'alt' TAB 'ref' CRLF" << endl;
+		cerr << "record  ::=  chrom TAB pos TAB alt TAB ref CRLF" << endl;
+		cerr << "chrom   ::=  ('chr' | '') ([0-9] | 'X' | 'Y' | 'MT')" << endl;
+		cerr << "pos     ::=  [0-9]+" << endl;
+		cerr << "alt     ::=  nucl" << endl;
+		cerr << "ref     ::=  nucl" << endl;
+		cerr << "nucl    ::=  'A' | 'C' | 'G' | 'T'" << endl;
+		return 1;
+	}
+
+	char* snv_path = argv[1];
+	char* bam_path = argv[2];
+	char* out_path = argv[3];
+
+	bool keep_dup = (atoi(argv[4]) != 0);
+	int min_mapq = atoi(argv[5]);
+	int min_baseq = atoi(argv[6]);
+
+
+	/// initialize query
+
+	fetcher f;
+
+	f.qfilter.min_mapq = min_mapq;
+	f.qfilter.min_baseq = min_baseq;
+
+	if (keep_dup) {
+		f.qfilter.disable_excl_flags(BAM_FDUP);
+	}
+	
+	f.open(bam_path);
+
+
+	/// initialize input file
+	
+	ifstream snvf(snv_path);
+
+	if (!snvf.is_open()) {
+		cerr << "ERROR: coudl not open for read: " << snv_path << endl;
+		return 1;
+	}
+
+	string line;
+	// discard header line
+	getline(snvf, line);
+
+
+	/// initialize output file
+	
+	ofstream outf(out_path);
+
+	if (!outf.is_open()) {
+		cerr << "ERROR: could not open for write: " << out_path << endl;
+		return 1;
+	}
+
+	// write header line
+	outf << "chrom\tpos\tref\talt\tref_count\talt_count" << endl;
+
+
+	/// process each input snv
+	
+	size_t snv_i = 0;
+
+	while (!snvf.eof()) {
+		getline(snvf, line);
+		if (line.empty()) break;
+		if (line[0] == '#') continue;
+
+		int32_t rid, pos;
+		nuc_t ref, alt;
+
+		read_snv_record(line, f, rid, pos, ref, alt);
+
+		// fetch all reads at query position without mate
+		f.fetch(rid, pos);
+
+		// tally the alleles at the query position
+		size_t n = f.pile.queries.size();
+		size_t nA = 0, nC = 0, nG = 0, nT = 0, nN = 0, nNull = 0, nDel = 0, nIns = 0;
+		for (size_t i = 0; i < n; ++i) {
+			bam1_t *b = f.pile.queries[i];
+			switch (query_nucleotide(b, pos)) {
+			case nuc_A:
+				++nA;
+				break;
+			case nuc_C:
+				++nC;
+				break;
+			case nuc_G:
+				++nG;
+				break;
+			case nuc_T:
+				++nT;
+				break;
+			case nuc_N:
+				++nN;
+				break;
+			case nuc_NULL:
+				++nNull;
+				break;
+			case nuc_DEL:
+				++nDel;
+				break;
+			case nuc_INS:
+				++nIns;
+				break;
+			}
+		}
+
+		int32_t ref_count;
+		switch (ref) {
+		case nuc_A:
+			ref_count = nA;
+			break;
+		case nuc_C:
+			ref_count = nC;
+			break;
+		case nuc_G:
+			ref_count = nG;
+			break;
+		case nuc_T:
+			ref_count = nT;
+			break;
+		default:
+			ref_count = 0;
+			break;
+		}
+
+		int32_t alt_count;
+		switch (alt) {
+		case nuc_A:
+			alt_count = nA;
+			break;
+		case nuc_C:
+			alt_count = nC;
+			break;
+		case nuc_G:
+			alt_count = nG;
+			break;
+		case nuc_T:
+			alt_count = nT;
+			break;
+		default:
+			alt_count = 0;
+			break;
+		}
+
+		// write output line
+		outf << line << '\t' << ref_count << '\t' << alt_count << endl;
+
+		f.clear();
+
+		++snv_i;
+	}
+
+	snvf.close();
+	outf.close();
+
+	return 0;
+}
---
+++ src/test-count.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+bam=../data/test.bam
+outdir=out/count
+testdir=test/count
+
+mkdir -p $outdir
+
+# do not keep duplicate
+./hts-count $testdir/snvs.tsv $bam $outdir/cov.tsv 0 0 0
+
+
+echo "Testing diff ..."
+
+diff ${outdir}/cov.tsv ${testdir}/cov.tsv
+echo "${outdir}/cov.tsv ... $?"
+
