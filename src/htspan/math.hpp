ed934ea14d9f4340a1080284fc9d07de332f3f53
David J. H. Shih <djh.shih@gmail.com>
2018-04-18 21:47:15 -0400

    Added log_sum_exp function.

---
+++ src/htspan/math.hpp
@@ -0,0 +1,41 @@
+#ifndef _HTSPAN_MATH_HPP_
+#define _HTSPAN_MATH_HPP_
+
+#include <cmath>
+
+namespace hts {
+
+using namespace std;
+
+/**
+ * Calculate log(sum(exp(xs))) of array xs.
+ *
+ * @param n   size of array
+ * @param xs  array
+ * @return log(sum(exp(xs)))
+ */
+double log_sum_exp(size_t n, double xs[]) {
+	if (n > 0) {
+		// find maximum value
+		double max = xs[0];
+		for (size_t i = 1; i < n; ++i) {
+			if (xs[i] > max) {
+				max = xs[i];
+			}
+		}
+
+		// calculate log sum exp
+		double sum = 0;
+		for (size_t i = 0; i < n; ++i) {
+			sum += exp(xs[i] - max);
+		}
+		return log(sum) + max;
+	} else {
+		return 0;
+	}
+}
+
+}  // namespace hts
+
+#endif  // HTSPAN_MATH_HPP_
+
