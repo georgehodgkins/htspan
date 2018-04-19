59de73adf161aec80bd566ac9a78fc78d2c6d23e
David J. H. Shih <djh.shih@gmail.com>
2018-04-18 21:47:03 -0400

    Fix tabs.

--- src/htspan/arg.hpp
+++ src/htspan/arg.hpp
@@ -9,31 +9,31 @@
 
 struct Arg: public option::Arg
 {
-    static option::ArgStatus Some(const option::Option& option, bool msg) {
-        if (option.arg != NULL) return option::ARG_OK;
-        if (msg) std::cerr << "Option `" << option.name << "` requires an argument" << std::endl;
-        return option::ARG_ILLEGAL;
-    }
+	static option::ArgStatus Some(const option::Option& option, bool msg) {
+		if (option.arg != NULL) return option::ARG_OK;
+		if (msg) std::cerr << "Option `" << option.name << "` requires an argument" << std::endl;
+		return option::ARG_ILLEGAL;
+	}
 
-    static option::ArgStatus InFile(const option::Option& option, bool msg) {
-        if (option.arg != NULL) {
-            if (std::strcmp(option.arg, "-") == 0 || file_exists(option.arg)) {
-                return option::ARG_OK;
-            }
-        }
-        if (msg) std::cerr << "Error: option `" << option.name << "` requires a valid input file" << std::endl;
-        return option::ARG_ILLEGAL;
-    }
+	static option::ArgStatus InFile(const option::Option& option, bool msg) {
+		if (option.arg != NULL) {
+			if (std::strcmp(option.arg, "-") == 0 || file_exists(option.arg)) {
+				return option::ARG_OK;
+			}
+		}
+		if (msg) std::cerr << "Error: option `" << option.name << "` requires a valid input file" << std::endl;
+		return option::ARG_ILLEGAL;
+	}
 
-    static option::ArgStatus OutFile(const option::Option& option, bool msg) {
-        if (option.arg != NULL) {
-            if (std::strcmp(option.arg, "-") == 0 || file_writable(option.arg)) {
-                return option::ARG_OK;
-            }
-        }
-        if (msg) std::cerr << "Error: option `" << option.name << "` requires a writable output path" << std::endl;
-        return option::ARG_ILLEGAL;
-    }
+	static option::ArgStatus OutFile(const option::Option& option, bool msg) {
+		if (option.arg != NULL) {
+			if (std::strcmp(option.arg, "-") == 0 || file_writable(option.arg)) {
+				return option::ARG_OK;
+			}
+		}
+		if (msg) std::cerr << "Error: option `" << option.name << "` requires a writable output path" << std::endl;
+		return option::ARG_ILLEGAL;
+	}
 };
 
 #endif  // _RGSAM_ARG_HPP_
--- src/htspan/file.hpp
+++ src/htspan/file.hpp
@@ -5,13 +5,13 @@
 #include <string>
 
 inline bool file_exists (const char* fname) {
-    std::ifstream f(fname);
-    return f.good();
+	std::ifstream f(fname);
+	return f.good();
 }
 
 inline bool file_writable (const char* fname) {
-    std::ofstream f(fname);
-    return f.good();
+	std::ofstream f(fname);
+	return f.good();
 }
 
 /**
@@ -20,27 +20,27 @@ inline bool file_writable (const char* fname) {
  * Assumes POSIX path.
  */
 void get_file_stem(const std::string& fname, std::string& stem) {
-    size_t start = fname.rfind("/");
-    if (start == std::string::npos) {
-        start = 0;
-    } else {
-        ++start;
-    }
+	size_t start = fname.rfind("/");
+	if (start == std::string::npos) {
+		start = 0;
+	} else {
+		++start;
+	}
 
-    size_t end = fname.rfind(".");
-    stem = fname.substr(start, end - start);
+	size_t end = fname.rfind(".");
+	stem = fname.substr(start, end - start);
 }
 
 /**
  * Get file extension.
  */
 void get_file_ext(const std::string& fname, std::string& ext) {
-    size_t start = fname.rfind(".");
-    if (start == std::string::npos || start == fname.length() - 1) {
-        ext = "";
-        return;
-    }
-    ext = fname.substr(start + 1);
+	size_t start = fname.rfind(".");
+	if (start == std::string::npos || start == fname.length() - 1) {
+		ext = "";
+		return;
+	}
+	ext = fname.substr(start + 1);
 }
 
 #endif  // _RGSAM_FILE_HPP_
