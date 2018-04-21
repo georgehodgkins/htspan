a477ed82de4cc4da5d3e8b53dee25c8ad78dd860
David J. H. Shih <djh.shih@gmail.com>
2018-04-21 16:55:35 -0400

    Use gslcblas

--- default.nix
+++ default.nix
@@ -2,5 +2,5 @@ with import <nixpkgs> {};
 stdenv.mkDerivation rec {
 	name = "env";
 	env = buildEnv { name = name; paths = buildInputs; };
-	buildInputs = [ gsl lzma bzip2 atlas ];
+	buildInputs = [ gsl lzma bzip2 ];
 }
