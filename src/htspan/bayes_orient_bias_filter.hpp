792198cb692f39661ce10a423189f49cf97c5e68
George Hodgkins <hodgkinsgeorge@gmail.com>
2019-06-12 08:29:53 -0500

    Changed grid generation to use fixed-allocation vector

--- src/htspan/bayes_orient_bias_filter.hpp
+++ src/htspan/bayes_orient_bias_filter.hpp
@@ -1,4 +1,4 @@
-#include <deque>
+#include <vector>
 #include <cmath>
 
 #include "orient_bias_filter.hpp"
@@ -15,49 +15,46 @@ struct bayes_orient_bias_filter_f : public orient_bias_filter_f {
 		: orient_bias_filter_f (_ref, _alt, n) {}
 
 	/**
-	* Generate a set of exponentially spaced points between zero and one
+	* Generate a vector of exponentially spaced points between zero and one
 	* to numerically integrate using the midpoint method. Points will be
-	* distributed, from the center, in steps of step*base^x, where x starts
+	* distributed, from the center, in steps of step*base^k, where k starts
 	* at zero and increases by 1 for each step.
 	*
 	* The set's smallest and largest members will be eps and 1-eps, respectively.
 	* It is also guaranteed to contain the center; with the possible exception
 	* of the endpoints, all points in the set will be symmetrical around this center.
 	*
-	* @param center The point which the grid will be symmetrical around
+	* @param center The x-value which the grid will be symmetrical around
 	* @param eps Defines the endpoints of the grid, [eps, 1-eps]
 	* @param step Coefficient of the exponential term in step generation (see above)
 	* @param base Base of the exponential term in step generation (see above)
-	* @return A std::deque containing the grid points
+	* @return A std::vector containing the grid points
 	*/
-	std::deque<double> generate_grid (double center, double eps = 1e-6, double step = .005, double base = 1.4) {
-		// initialize a deque containing only the center value
-		std::deque<double> grid_deck(1, center);
-		double exp = 0.0;
-		double off = step*pow(base, exp);
-		// successively add values to each end of the deque,
-		// symmetrically offset from the center
-		while (center - off > eps && center + off < 1 - eps) {
-			grid_deck.push_back(center + off);
-			grid_deck.push_front(center - off);
-			++exp;
-			off = step*pow(base, exp);
+	std::vector<double> generate_grid (double center, double eps = 1e-6, double step = .005, double base = 1.4) {
+		// calculate number of grid points to allocate
+		// log_b is log base b
+		// c+s*b^k < 1 - eps ---> k < log_b ((1-eps-c)/2) 
+		size_t right_points = (size_t) floor(log((1-eps-center)/step)/log(base));
+		// c-s*b^k > eps ---> k < log_b((c-eps)/s)
+		size_t left_points = (size_t) floor(log((center - eps)/step)/log(base));
+		// three extra points: center and two endpoints
+		size_t v_cap = right_points + left_points + 3;
+		// allocate vector
+		std::vector<double> grid (v_cap);
+		// place fixed points
+		grid[0] = eps;
+		grid[v_cap - 1] = 1 - eps;
+		// first point is eps, so last generated point on left is at index left_points
+		grid[left_points + 1] = center;
+		// generate left points
+		for (size_t k = 0; k <= left_points; ++k) {
+			grid[left_points - k] = step * pow(base, k);
 		}
-		// ensure that both ends are completed (only one loop will execute)
-		while (center - off > eps) {
-			grid_deck.push_front(center - off);
-			++exp;
-			off = step*pow(base, exp);
+		// generate right points
+		for (size_t k = 0; k <= right_points; ++k) {
+			grid[left_points + 2 + k] = step * pow(base, k);
 		}
-		while (center + off < 1 - eps) {
-			grid_deck.push_back(center + off);
-			++exp;
-			off = step*pow(base, exp);
-		}
-		// Add eps and 1-eps as endpoints
-		grid_deck.push_front(eps);
-		grid_deck.push_back(1 - eps);
-		return grid_deck;
+		return grid;
 	}
 
 	
