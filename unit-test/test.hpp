9d129be5e69b601b802ed9dd14b8eecff2bdc6a5
George Hodgkins <hodgkinsgeorge@gmail.com>
2019-06-19 15:06:02 -0500

    Changed to order-of-magnitude standard for small value unit test

--- unit-test/test.hpp
+++ unit-test/test.hpp
@@ -3,10 +3,13 @@
 #ifndef _HTSPAN_TEST_HPP_
 #define _HTSPAN_TEST_HPP_
 
-#define TEST_EPS 1e-4
+#include <cmath>
+
+#define TEST_EPS 1e-3
 #define TEST_RAT .5
 
 // Returns the difference ratio of two values, the distance between the values divided by their mean
+// not used at the moment
 inline double diff_rat (double a, double b) {
 	return 2.0*abs(a-b)/(a+b);
 }
@@ -14,15 +17,15 @@ inline double diff_rat (double a, double b) {
 // Differentiated tests for values smaller than the test eps
 // True if the test passes
 bool test_val(double got, double std) {
-	if (got < TEST_EPS || std < TEST_EPS) {
+	if (abs(got) < TEST_EPS || abs(std) < TEST_EPS) {
 		// tests for values extremely close to zero
 		if (got == 0.0) {
-			return std < TEST_EPS*TEST_EPS;
+			return abs(std) < TEST_EPS*TEST_EPS;
 		}
 		if (std == 0.0) {
-			return got < TEST_EPS*TEST_EPS;
+			return abs(got) < TEST_EPS*TEST_EPS;
 		}
-		return diff_rat(got, std) < TEST_RAT;
+		return (got/std < 10) && (got/std > .1);
 	} else {
 		return abs(got-std) < TEST_EPS;
 	}
