b0de9d295e2fff1a460ab5b2f712f5a040cedc7c
George Hodgkins <hodgkinsgeorge@gmail.com>
2019-06-20 10:21:48 -0500

    Added VCF reader to snv.hpp and improved snv reader error reporting

--- src/htspan/io/snv.hpp
+++ src/htspan/io/snv.hpp
@@ -19,29 +19,44 @@ using namespace std;
 namespace snv {
 
 struct record {
+	// HTS identifier of reference sequence
 	int32_t rid;
+	// Human-readable name of reference sequence
 	string chrom;
+	// Read reference position
 	int32_t pos;
+	// Reference nucleotide
 	nuc_t nt_ref;
+	// Alternate nucleotide
 	nuc_t nt_alt;
+	/**
+	* Non-fatal error codes:
+	* 0=OK
+	* 1=could not find RID given in TSV
+	* 2=could not unpack VCF 
+	* 3=multiple-nucleotide variant in VCF
+	*/
+	// Note that since vcf_read() returns the same value for EOF and other read errors, we cannot easily catch errors there
+	int32_t err;
 };
 
-struct snv_reader {
+struct reader {
+
 	virtual bool next (record &r) {}
 
 	virtual void close ()
 };
 
-struct tsv_reader : snv_reader {
+struct tsv_reader : reader {
 	ifstream f;
 
 	bam_hdr_t *hdr;
 
 	string line;
 
-	reader(const char* path, bam_hdr_t *h) : f(path), h(hdr) {
+	tsv_reader(const char* path, bam_hdr_t *h) : f(path), h(hdr) {
 	 	if (!f.is_open()) {
-	 		throw runtime_error("Error: could not open input file.");
+	 		throw runtime_error("Error: could not open input TSV file.");
 	 	}
 
 	 	// discard header line
@@ -69,16 +84,82 @@ struct tsv_reader : snv_reader {
 		r.nt_alt = char_to_nuc(char_alt);
 
 		// lookup rid in the attached BAM header
-		// note that calling code should handle the failure condition (rid == -1)
 		r.rid = bam_name_to_id(hdr, rec.chrom);
+		if (r.rid == -1) {
+			r.err = 1;
+			return true;
+		}
 
+		r.err = 0;
 		return true;
 	}
 
 	void close() {
 		f.close();
+		hdr = NULL;
 	}
 };
+
+struct vcf_reader : reader {
+	// pointer to main VCF/BCF file object
+	htsFile *hf;
+	// pointer to VCF/BCF header object
+	const bcf_hdr_t *hdr;
+	// pointer to VCF record buffer to read to/from
+	bcf1_t *v;
+
+	vcf_reader(const char* path) {
+		// open HTS file handle
+		hf = hts_open(path, "r");
+		if (!hf) {
+			throw runtime_error("Could not open input VCF file.");
+		}
+
+		// read VCF/BCF header from file
+		hdr = bcf_hdr_read(hf);
+		if (!hdr) {
+			throw runtime_error("Could not read header from VCF file.");
+		}
+
+		// allocate space for VCF record
+		v = bcf_init();
+	}
+
+	bool next(record &r) {
+		// read in and unpack the record
+		int status = bcf_read1(hf, hdr, v);
+		if (status == -1) {// EOF or other reading error
+			return false;
+		}
+		status = bcf_unpack(v, BCF_UN_STR);
+		if (status < 0) {
+			r.err = 2;
+			return true;
+		}
+		// This class only handles SNVs
+		if (v->rlen > 1) {
+			r.err = 3;
+			return true;
+		}
+		// fill record fields
+		// TODO: find a way to get the chrom name from the rid
+		r.rid = v->rid;
+		r.chrom = "VCF-unset";
+		r.pos = v->pos - 1;// convert from 1-based to 0-based
+		r.nt_ref = char_to_nuc(v->d.allele[0][0]);
+		r.nt_alt = char_to_nuc(v->d.allele[1][0]);
+		r.err = 0;
+		return true;
+	}
+
+	void close() {
+		hts_close(hf);
+		bcf_destroy1(v);
+		bcf_hdr_destroy(hdr);
+	}
+}
+
+
 
 }  // namespace snv
 
