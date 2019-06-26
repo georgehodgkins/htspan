31cb46b7c0e198d4c0b481e3ef8ada060f6f3232
David J. H. Shih <djh.shih@gmail.com>
2019-06-26 12:24:15 -0500

    Complete read simulation and alignment script.

--- data/simulate.sh
+++ data/simulate.sh
@@ -1,5 +1,23 @@
-bwa index TP53_hg38.fasta
-bwa mem TP53_hg38.fasta reads_tp53.fastq | samtools view -b > reads_tp53.raw.bam
-samtools sort reads_tp53.raw.bam -o reads_tp53.bam
-rm reads_tp53.raw.bam
+
+ref=tp53_hg38.fasta
+out=sim
+
+r1=$out.r1.fq
+r2=$out.r2.fq
+
+# 2*100 bp reads * 10000 reads / 19149 bp seq = 104x coverage
+wgsim -h -1 100 -2 100 -S 0 -e 0.05 -N 10000 -R 0 \
+	$ref $r1 $r2 | 
+	cut -f 1-4 > $out.snv
+
+# Align reads
+
+if [[ ! -f ${ref}.bwt ]]; then
+	bwa index ${ref}
+fi
+
+bwa mem $ref $r1 $r2 | 
+	samtools view -b > $out.raw.bam
+samtools sort $out.raw.bam -o $out.bam
+rm $out.raw.bam
 
