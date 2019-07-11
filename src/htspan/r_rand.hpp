97a5e4b445e80f2a40b826127e788960843ea585
George Hodgkins <hodgkinsgeorge@gmail.com>
2019-07-11 15:47:20 -0500

    Replaced rbeta() and rbinom() implementations with those from R source

---
+++ src/htspan/r_rand.hpp
@@ -0,0 +1,348 @@
+#ifndef _HTSPAN_R_RAND_HPP_
+#define _HTSPAN_R_RAND_HPP_
+
+// This header contains random sampling methods from the R source code,
+// for binomial and beta distributed variates
+
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
+ *  Copyright (C) 2000--2016 The R Core Team
+ *  Copyright (C) 2001--2016 The R Foundation
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, a copy is available at
+ *  https://www.R-project.org/Licenses/
+ */
+
+#include <cmath>
+#include <cfloat>
+
+#include <alglib/alglibmisc.h>
+
+namespace hts {
+
+namespace r_rand {
+
+using namespace std;
+// These methods are taken from the OpenCV source at
+// modules/hal/include/opencv2/hal/defs.h
+bool isinf(double x)
+{
+    union { uint64_t u; double f; } ieee754;
+    ieee754.f = x;
+    return ( (unsigned)(ieee754.u >> 32) & 0x7fffffff ) == 0x7ff00000 &&
+           ( (unsigned)ieee754.u == 0 );
+}
+
+bool isnan(double x)
+{
+    union { uint64_t u; double f; } ieee754;
+    ieee754.f = x;
+    return ( (unsigned)(ieee754.u >> 32) & 0x7fffffff ) +
+           ( (unsigned)ieee754.u != 0 ) > 0x7ff00000;
+}
+
+double fmin2(double x, double y) {
+	return (x < y) ? x : y;
+}
+
+double fmax2(double x, double y) {
+	return (x > y) ? x : y;
+}
+
+
+#define unif_rand() alglib::hqrnduniformr(rng)
+
+// This method is from R source at
+// src/nmath/rbeta.c, with cosmetic changes for compatibility
+double rbeta(double aa, double bb)
+{
+	static alglib::hqrndstate rng;
+	alglib::hqrndrandomize(rng);
+    if (isnan(aa) || isnan(bb) || aa < 0. || bb < 0.) {
+    	throw invalid_argument("NaN passed to rbeta()");
+    }
+    if (isinf(aa) && isinf(bb)) // a = b = Inf : all mass at 1/2
+	return 0.5;
+    if (aa == 0. && bb == 0.) // point mass 1/2 at each of {0,1} :
+	return (unif_rand() < 0.5) ? 0. : 1.;
+    // now, at least one of a, b is finite and positive
+    if (isinf(aa) || bb == 0.)
+    	return 1.0;
+    if (isinf(bb) || aa == 0.)
+    	return 0.0;
+
+    double a, b, alpha;
+    double r, s, t, u1, u2, v, w, y, z;
+    int qsame;
+    /* FIXME:  Keep Globals (properly) for threading */
+    /* Uses these GLOBALS to save time when many rv's are generated : */
+    static double beta, gamma, delta, k1, k2;
+    static double olda = -1.0;
+    static double oldb = -1.0;
+
+    /* Test if we need new "initializing" */
+    qsame = (olda == aa) && (oldb == bb);
+    if (!qsame) { olda = aa; oldb = bb; }
+
+    a = fmin2(aa, bb);
+    b = fmax2(aa, bb); /* a <= b */
+    alpha = a + b;
+
+#define v_w_from__u1_bet(AA) 			\
+	    v = beta * log(u1 / (1.0 - u1));	\
+	    if (v <= DBL_MAX) {			\
+		w = AA * exp(v);		\
+		if(isinf(w)) w = DBL_MAX;	\
+	    } else				\
+		w = DBL_MAX
+
+
+    if (a <= 1.0) {	/* --- Algorithm BC --- */
+
+	/* changed notation, now also a <= b (was reversed) */
+
+	if (!qsame) { /* initialize */
+	    beta = 1.0 / a;
+	    delta = 1.0 + b - a;
+	    k1 = delta * (0.0138889 + 0.0416667 * a) / (b * beta - 0.777778);
+	    k2 = 0.25 + (0.5 + 0.25 / delta) * a;
+	}
+	/* FIXME: "do { } while()", but not trivially because of "continue"s:*/
+	for(;;) {
+	    u1 = unif_rand();
+	    u2 = unif_rand();
+	    if (u1 < 0.5) {
+		y = u1 * u2;
+		z = u1 * y;
+		if (0.25 * u2 + z - y >= k1)
+		    continue;
+	    } else {
+		z = u1 * u1 * u2;
+		if (z <= 0.25) {
+		    v_w_from__u1_bet(b);
+		    break;
+		}
+		if (z >= k2)
+		    continue;
+	    }
+
+	    v_w_from__u1_bet(b);
+
+	    if (alpha * (log(alpha / (a + w)) + v) - 1.3862944 >= log(z))
+		break;
+	}
+	return (aa == a) ? a / (a + w) : w / (a + w);
+
+    }
+    else {		/* Algorithm BB */
+
+	if (!qsame) { /* initialize */
+	    beta = sqrt((alpha - 2.0) / (2.0 * a * b - alpha));
+	    gamma = a + 1.0 / beta;
+	}
+	do {
+	    u1 = unif_rand();
+	    u2 = unif_rand();
+
+	    v_w_from__u1_bet(a);
+
+	    z = u1 * u1 * u2;
+	    r = gamma * v - 1.3862944;
+	    s = a + r - w;
+	    if (s + 2.609438 >= 5.0 * z)
+		break;
+	    t = log(z);
+	    if (s > t)
+		break;
+	}
+	while (r + alpha * log(alpha / (b + w)) < t);
+
+	return (aa != a) ? b / (b + w) : w / (b + w);
+    }
+}
+
+#define repeat for(;;)
+
+
+// This method is taken from R source at
+// https://github.com/wch/r-source/blob/trunk/src/nmath/rbinom.c, with minor changes for compatibility
+int rbinom(int nin, double pp)
+{
+	static alglib::hqrndstate rng;
+	alglib::hqrndrandomize(rng);
+
+    static double c, fm, npq, p1, p2, p3, p4, qn;
+    static double xl, xll, xlr, xm, xr;
+
+    static double psave = -1.0;
+    static int nsave = -1;
+    static int m;
+
+    double f, f1, f2, u, v, w, w2, x, x1, x2, z, z2;
+    double p, q, np, g, r, al, alv, amaxp, ffm, ynorm;
+    int i, ix, k, n;
+
+    r = (double) nin;
+    if (isinf(pp) ||
+	/* n=0, p=0, p=1 are not errors <TSL>*/
+	r < 0 || pp < 0. || pp > 1.) {
+		throw invalid_argument("NaN passed to rbinom()");
+	}
+
+    if (r == 0 || pp == 0.) return 0;
+    if (pp == 1.) return nin;
+
+    /* else */
+    n = nin;
+
+    p = fmin2(pp, 1. - pp);
+    q = 1. - p;
+    np = n * p;
+    r = p / q;
+    g = r * (n + 1);
+
+    /* Setup, perform only when parameters change [using static (globals): */
+
+    /* FIXING: Want this thread safe
+       -- use as little (thread globals) as possible
+    */
+    if (pp != psave || n != nsave) {
+	psave = pp;
+	nsave = n;
+	if (np < 30.0) {
+	    /* inverse cdf logic for mean less than 30 */
+	    qn = pow(q, n);
+	    goto L_np_small;
+	} else {
+	    ffm = np + p;
+	    m = (int) ffm;
+	    fm = m;
+	    npq = np * q;
+	    p1 = (int)(2.195 * sqrt(npq) - 4.6 * q) + 0.5;
+	    xm = fm + 0.5;
+	    xl = xm - p1;
+	    xr = xm + p1;
+	    c = 0.134 + 20.5 / (15.3 + fm);
+	    al = (ffm - xl) / (ffm - xl * p);
+	    xll = al * (1.0 + 0.5 * al);
+	    al = (xr - ffm) / (xr * q);
+	    xlr = al * (1.0 + 0.5 * al);
+	    p2 = p1 * (1.0 + c + c);
+	    p3 = p2 + c / xll;
+	    p4 = p3 + c / xlr;
+	}
+    } else if (n == nsave) {
+	if (np < 30.0)
+	    goto L_np_small;
+    }
+
+    /*-------------------------- np = n*p >= 30 : ------------------- */
+    repeat {
+      u = unif_rand() * p4;
+      v = unif_rand();
+      /* triangular region */
+      if (u <= p1) {
+	  ix = (int)(xm - p1 * v + u);
+	  goto finis;
+      }
+      /* parallelogram region */
+      if (u <= p2) {
+	  x = xl + (u - p1) / c;
+	  v = v * c + 1.0 - fabs(xm - x) / p1;
+	  if (v > 1.0 || v <= 0.)
+	      continue;
+	  ix = (int) x;
+      } else {
+	  if (u > p3) {	/* right tail */
+	      ix = (int)(xr - log(v) / xlr);
+	      if (ix > n)
+		  continue;
+	      v = v * (u - p3) * xlr;
+	  } else {/* left tail */
+	      ix = (int)(xl + log(v) / xll);
+	      if (ix < 0)
+		  continue;
+	      v = v * (u - p2) * xll;
+	  }
+      }
+      /* determine appropriate way to perform accept/reject test */
+      k = abs(ix - m);
+      if (k <= 20 || k >= npq / 2 - 1) {
+	  /* explicit evaluation */
+	  f = 1.0;
+	  if (m < ix) {
+	      for (i = m + 1; i <= ix; i++)
+		  f *= (g / i - r);
+	  } else if (m != ix) {
+	      for (i = ix + 1; i <= m; i++)
+		  f /= (g / i - r);
+	  }
+	  if (v <= f)
+	      goto finis;
+      } else {
+	  /* squeezing using upper and lower bounds on log(f(x)) */
+	  amaxp = (k / npq) * ((k * (k / 3. + 0.625) + 0.1666666666666) / npq + 0.5);
+	  ynorm = -k * k / (2.0 * npq);
+	  alv = log(v);
+	  if (alv < ynorm - amaxp)
+	      goto finis;
+	  if (alv <= ynorm + amaxp) {
+	      /* stirling's formula to machine accuracy */
+	      /* for the final acceptance/rejection test */
+	      x1 = ix + 1;
+	      f1 = fm + 1.0;
+	      z = n + 1 - fm;
+	      w = n - ix + 1.0;
+	      z2 = z * z;
+	      x2 = x1 * x1;
+	      f2 = f1 * f1;
+	      w2 = w * w;
+	      if (alv <= xm * log(f1 / x1) + (n - m + 0.5) * log(z / w) + (ix - m) * log(w * p / (x1 * q)) + (13860.0 - (462.0 - (132.0 - (99.0 - 140.0 / f2) / f2) / f2) / f2) / f1 / 166320.0 + (13860.0 - (462.0 - (132.0 - (99.0 - 140.0 / z2) / z2) / z2) / z2) / z / 166320.0 + (13860.0 - (462.0 - (132.0 - (99.0 - 140.0 / x2) / x2) / x2) / x2) / x1 / 166320.0 + (13860.0 - (462.0 - (132.0 - (99.0 - 140.0 / w2) / w2) / w2) / w2) / w / 166320.)
+		  goto finis;
+	  }
+      }
+  }
+
+ L_np_small:
+    /*---------------------- np = n*p < 30 : ------------------------- */
+
+  repeat {
+     ix = 0;
+     f = qn;
+     u = unif_rand();
+     repeat {
+	 if (u < f)
+	     goto finis;
+	 if (ix > 110)
+	     break;
+	 u -= f;
+	 ix++;
+	 f *= (g / ix - r);
+     }
+  }
+ finis:
+    if (psave > 0.5)
+	 ix = n - ix;
+  return ix;
+}
+
+#undef unif_rand
+#undef repeat
+
+} // namespace r_rand
+
+} // namespace hts
+
+#endif //_HTSPAN_R_RAND_HPP_
