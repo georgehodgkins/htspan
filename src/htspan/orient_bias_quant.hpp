f10ee679ca9b436f2700aafc2709fc370fcd2487
George Hodgkins <hodgkinsgeorge@gmail.com>
2019-07-13 14:13:11 -0500

    Changed optimization to log space and refactored sim code

--- src/htspan/orient_bias_quant.hpp
+++ src/htspan/orient_bias_quant.hpp
@@ -34,6 +34,21 @@ struct hparams {
 	double beta_theta;
 	double alpha_phi;
 	double beta_phi;
+
+	// Return parameter vectors for theta or phi
+	vector<double> theta_pvec() {
+		vector<double> rtn (2);
+		rtn[0] = alpha_theta;
+		rtn[1] = beta_theta;
+		return rtn;
+	}
+
+	vector<double> phi_pvec() {
+		vector<double> rtn (2);
+		rtn[0] = alpha_phi;
+		rtn[1] = beta_phi;
+		return rtn;
+	}
 };
 
 /**
@@ -142,28 +157,24 @@ struct base_orient_bias_quant_f {
 	}
 
 	/*
-	* Simulate a correctly distributed set of observed variables, for testing.
+	* Simulate a correctly distributed set of observed variables for a read, for testing.
+	*
+	* @param N Number of reads at the locus
+	* @param theta Alternate allele probability at the locus
+	* @param phi Damage probability at the locus
+	* @param xc returned as damage-consistent alt count at the locus
+	* @param xi returned as damage-inconsistent alt count at the locus
+	* @param nc returned as correctly oriented alt count at the locus
+	* @param ni returned as incorrectly oriented alt count at the locus
 	*/
-	// TODO: Change this to do a single read
-	static void simulate_orient_bias_read_counts (const vector<size_t> &ns_vec,
-			const vector<double> &theta_vec, const vector<double> &phi_vec,
-			vector<long int> &xc_vec, vector<long int> &xi_vec, vector<long int> &nc_vec, vector<long int> &ni_vec) {
-		// setup return vectors
-		xc_vec.clear();
-		xc_vec.reserve(ns_vec.size());
-		xi_vec.clear();
-		xi_vec.reserve(ns_vec.size());
-		nc_vec.clear();
-		nc_vec.reserve(ns_vec.size());
-		ni_vec.clear();
-		ni_vec.reserve(ns_vec.size());
-		for (size_t j = 0; j < ns_vec.size(); ++j) {
-			ni_vec.push_back(ns_vec[j]/2);
-			nc_vec.push_back(ns_vec[j] - ni_vec[j]);
-			xi_vec.push_back(r_rand::rbinom(ni_vec[j], theta_vec[j]));
-			long int xr = r_rand::rbinom(nc_vec[j], theta_vec[j]);
-			xc_vec.push_back(xr + r_rand::rbinom(nc_vec[j] - xr, phi_vec[j])); // xd ~ Binom(nc, phi) and xc = xr + xd
-		}
+	static void simulate_orient_bias_read_counts (const size_t N, const double theta, const double phi,
+			long int &xc, long int &xi, long int &nc, long int &ni) {
+		ni = N/2;
+		nc = N - ni;
+		xi = r_rand::rbinom(ni, theta);
+		long int xr = r_rand::rbinom(nc, theta);
+		long int xd = r_rand::rbinom(nc - xr, phi);
+		xc = xr + xd;
 	}
 
 	/**
@@ -234,18 +245,7 @@ struct freq_orient_bias_quant_f : public base_orient_bias_quant_f {
 	* @param phi Global damage rate
 	*/
 	void simulate(size_t N, double theta, double phi) {
-		vector<size_t> ns_vec (1, N);
-		vector<double> theta_vec (1, theta);
-		vector<double> phi_vec (1, phi);
-		vector<long int> xc_vec;
-		vector<long int> xi_vec;
-		vector<long int> nc_vec;
-		vector<long int> ni_vec;
-		simulate_orient_bias_read_counts(ns_vec, theta_vec, phi_vec, xc_vec, xi_vec, nc_vec, ni_vec);
-		xc = xc_vec.front();
-		xi = xi_vec.front();
-		nc = nc_vec.front();
-		ni = ni_vec.front();
+		simulate_orient_bias_read_counts(N, theta, phi, xc, xi, nc, ni);
 	}
 
 	/**
@@ -314,6 +314,23 @@ struct bayes_orient_bias_quant_f : public base_orient_bias_quant_f {
 	}
 
 	/*
+	* Clear data vectors and reserve the requested
+	* number of entries in them.
+	*
+	* @param N Number of entries to reserve
+	*/ 
+	void reset_realloc (size_t N) {
+		xc_vec.clear();
+		xc_vec.reserve(N);
+		xi_vec.clear();
+		xi_vec.reserve(N);
+		nc_vec.clear();
+		nc_vec.reserve(N);
+		ni_vec.clear();
+		ni_vec.reserve(N);
+	}
+
+	/*
 	* Returns the number of processed loci (not total reads).
 	* For total reads, use n_reads().
 	*/
@@ -334,18 +351,25 @@ struct bayes_orient_bias_quant_f : public base_orient_bias_quant_f {
 	void simulate(const vector<size_t> &ns_vec, double alpha_theta, double beta_theta,
 			double alpha_phi, double beta_phi) {
 		vector<double> theta_vec (ns_vec.size());
-		vector<double> phi_vec(ns_vec.size());
+		vector<double> phi_vec (ns_vec.size());
+		// Generate correctly distributed values of phi and theta
+		// to pass to the simulator
 		for (size_t j = 0; j < ns_vec.size(); ++j) {
 			theta_vec[j] = r_rand::rbeta(alpha_theta, beta_theta);
 		}
 		for (size_t j = 0; j < ns_vec.size(); ++j) {
 			phi_vec[j] = r_rand::rbeta(alpha_phi, beta_phi);
 		}
-		simulate_orient_bias_read_counts(ns_vec, theta_vec, phi_vec, xc_vec, xi_vec, nc_vec, ni_vec);
+		// Simulate as many reads as necessary
+		reset_realloc(ns_vec.size());
+		for (size_t j = 0; j < ns_vec.size(); ++j) {
+			simulate_orient_bias_read_counts(ns_vec[j], theta_vec[j], phi_vec[j], xc, xi, nc, ni);
+			xc_vec.push_back(xc);
+			xi_vec.push_back(xi);
+			nc_vec.push_back(nc);
+			ni_vec.push_back(ni);
+		}
 	}
-
-	// The stepper type to use for stochastic gradient optimization
-	typedef stograd::stepper::adam<double> bayes_stepper_t;
 
 	/*
 	* Estimate the alpha and beta parameters that characterize
@@ -353,6 +377,7 @@ struct bayes_orient_bias_quant_f : public base_orient_bias_quant_f {
 	*
 	* Uses stochastic gradient optimization method (stograd).
 	*
+	* @template bayes_stepper_t stograd::stepper object to use for optimization
 	* @param bsize Batch size for stograd
 	* @param nepochs Number of epochs for stograd
 	* @param learning_rate Learning rate for stograd
@@ -363,6 +388,7 @@ struct bayes_orient_bias_quant_f : public base_orient_bias_quant_f {
 	* @param beta0_phi Initial estimate of beta_phi
 	* @return A hts::hparams object containing estimates of the four hyperparameters
 	*/
+	template <typename bayes_stepper_t>
 	hparams operator()(size_t bsize, size_t nepochs, double learning_rate, double eps,
 			double alpha0_theta, double beta0_theta, double alpha0_phi, double beta0_phi) {
 		vector<double> theta_init(2);
@@ -385,39 +411,18 @@ struct bayes_orient_bias_quant_f : public base_orient_bias_quant_f {
 	}
 
 	/*
-	* Evaluate the theta objective function (lp_xi_given_hparams) at
+	* Evaluate the theta objective function (-lp_xi_given_hparams) at
 	* the alpha_theta and beta_theta in the passed hparams object,
 	* taking into account read sites up to maxJ [default: all].
 	*/
 	double eval_theta_objective_func (hparams x, int maxJ = -1) {
-		vector<double> X (2);
-		X[0] = x.alpha_theta;
-		X[1] = x.beta_theta;
-		theta_hparams_optimizable F (*this, X);
+		theta_hparams_optimizable F (*this, x.theta_pvec());
 		if (maxJ == -1) {
 			F.J = size()-1;
 		} else {
 			F.J = maxJ;
 		}
-		return F(X);
-	}
-
-	/*
-	* Evaluate the theta gradient wrt beta_theta at
-	* the alpha_theta and beta_theta in the passed hparams object,
-	* taking into account read sites up to maxJ [default: all].
-	*/
-	double eval_theta_grad_dalpha (hparams x, int maxJ = -1) {
-		vector<double> X (2);
-		X[0] = x.alpha_theta;
-		X[1] = x.beta_theta;
-		theta_hparams_optimizable F(*this, X);
-		if (maxJ == -1) {
-			F.J = size()-1;
-		} else {
-			F.J = maxJ;
-		}
-		return F.dlp_xi_given_hparams_dalpha(x.alpha_theta, x.beta_theta);
+		return F(x.theta_pvec(), false);
 	}
 
 	/*
@@ -425,35 +430,44 @@ struct bayes_orient_bias_quant_f : public base_orient_bias_quant_f {
 	* the alpha_theta and beta_theta in the passed hparams object,
 	* taking into account read sites up to maxJ [default: all].
 	*/
-	double eval_theta_grad_dbeta (hparams x, int maxJ = -1) {
-		vector<double> X (2);
-		X[0] = x.alpha_theta;
-		X[1] = x.beta_theta;
-		theta_hparams_optimizable F(*this, X);
+	double eval_theta_grad_dalpha (hparams x, int maxJ = -1) {
+		theta_hparams_optimizable F(*this, x.theta_pvec());
 		if (maxJ == -1) {
 			F.J = size()-1;
 		} else {
 			F.J = maxJ;
 		}
-		return F.dlp_xi_given_hparams_dbeta(x.alpha_theta, x.beta_theta);
+		return F.dlp_xi_given_hparams_dalpha(x.theta_pvec(), false);
 	}
 
 	/*
-	* Evaluate the phi objective function (lp_xc_given_hparams) at
+	* Evaluate the theta gradient wrt beta_theta at
+	* the alpha_theta and beta_theta in the passed hparams object,
+	* taking into account read sites up to maxJ [default: all].
+	*/
+	double eval_theta_grad_dbeta (hparams x, int maxJ = -1) {
+		theta_hparams_optimizable F(*this, x.theta_pvec());
+		if (maxJ == -1) {
+			F.J = size()-1;
+		} else {
+			F.J = maxJ;
+		}
+		return F.dlp_xi_given_hparams_dbeta(x.theta_pvec(), false);
+	}
+
+	/*
+	* Evaluate the phi objective function (-lp_xc_given_hparams) at
 	* the point indicated by the passed hparams object,
 	* taking into account read sites up to maxJ [default: all].
 	*/
 	double eval_phi_objective_func (hparams x, int maxJ = -1) {
-		vector<double> X (2);
-		X[0] = x.alpha_phi;
-		X[1] = x.beta_phi;
-		phi_hparams_optimizable F(*this, X, x.alpha_theta, x.beta_theta);
+		phi_hparams_optimizable F(*this, x.phi_pvec(), x.alpha_theta, x.beta_theta);
 		if (maxJ == -1) {
 			F.J = size()-1;
 		} else {
 			F.J = maxJ;
 		}
-		return F(X);
+		return F(x.phi_pvec(), false);
 	}
 
 	/*
@@ -471,10 +485,11 @@ struct bayes_orient_bias_quant_f : public base_orient_bias_quant_f {
 		ni_vec = new_ni;
 	}
 
-	// TODO: Remove parent ref from optimizables
-
 	/*
 	* Optimizable functor for use in stograd.
+	* 
+	* Optimizes the alpha and beta parameters of the 
+	* beta distribution that characterizes theta.
 	*/
 	struct theta_hparams_optimizable {
 
@@ -485,20 +500,21 @@ struct bayes_orient_bias_quant_f : public base_orient_bias_quant_f {
 		size_t J;
 
 		// vector of parameters being optimized
+		// values are in unconstrained (log) space
 		// [0] = alpha_theta
 		// [1] = beta_theta
 		vector<double> curr_params;
 
 		double alpha() const {
-			return curr_params[0];
+			return exp(curr_params[0]);
 		}
 
 		double beta() const {
-			return curr_params[1];
+			return exp(curr_params[1]);
 		}
 
-		theta_hparams_optimizable (bayes_orient_bias_quant_f &P, vector<double> &initial_params) :
-			parent(P), J(0), curr_params(initial_params) {}
+		theta_hparams_optimizable (bayes_orient_bias_quant_f &P, vector<double> initial_params) :
+			parent(P), J(0), curr_params(log_c(initial_params)) {}
 
 		/*
 		* Total number of loci for which data is 
@@ -541,37 +557,46 @@ struct bayes_orient_bias_quant_f : public base_orient_bias_quant_f {
 			return alglib::psi(beta_theta + nij - xij) - alglib::psi(alpha_theta + beta_theta + nij);
 		}
 
-		double dlp_xi_given_hparams_dalpha (const double alpha_theta, const double beta_theta) {
+		double dlp_xi_given_hparams_dalpha (vector<double> x, bool exp_args = true) {
 			static double alpha_cached = -1.0;
 			static double psi_alpha_cached = 0.0;
-			if (alpha_theta != alpha_cached) {
-				psi_alpha_cached = alglib::psi(alpha_theta);
-				alpha_cached = alpha_theta;
+			if (exp_args) {
+				exp_elements(x);
+			}
+			if (x[0] != alpha_cached) {
+				psi_alpha_cached = alglib::psi(x[0]);
+				alpha_cached = x[0];
 			}
 			// xi_vec and ni_vec are class members
 			double sum_dlp_xij_dalpha = 0.0;
 			for (size_t j = 0; j < cobs(); ++j) {
-				sum_dlp_xij_dalpha += dlp_xij_given_hparams_dalpha(parent.xi_vec[j], parent.ni_vec[j], alpha_theta, beta_theta);
+				sum_dlp_xij_dalpha += dlp_xij_given_hparams_dalpha(parent.xi_vec[j], parent.ni_vec[j], x[0], x[1]);
 			}
-			return sum_dlp_xij_dalpha + cobs()*(alglib::psi(alpha_theta + beta_theta) - psi_alpha_cached);
+			return sum_dlp_xij_dalpha + cobs()*(alglib::psi(x[0] + x[1]) - psi_alpha_cached);
 		}
 
-		double dlp_xi_given_hparams_dbeta (const double alpha_theta, const double beta_theta) {
+		double dlp_xi_given_hparams_dbeta (vector<double> x, bool exp_args = true) {
 			static double beta_cached = -1.0;
 			static double psi_beta_cached = 0.0;
-			if (beta_theta != beta_cached) {
-				psi_beta_cached = alglib::psi(beta_theta);
-				beta_cached = beta_theta;
+			if (exp_args) {
+				exp_elements(x);
+			}
+			if (x[1] != beta_cached) {
+				psi_beta_cached = alglib::psi(x[1]);
+				beta_cached = x[1];
 			}
 			double sum_dlp_xij_dbeta = 0.0;
 			for (size_t j = 0; j < cobs(); ++j) {
-				sum_dlp_xij_dbeta += dlp_xij_given_hparams_dbeta(parent.xi_vec[j], parent.ni_vec[j], alpha_theta, beta_theta);
+				sum_dlp_xij_dbeta += dlp_xij_given_hparams_dbeta(parent.xi_vec[j], parent.ni_vec[j], x[0], x[1]);
 			}
-			return sum_dlp_xij_dbeta + cobs()*(alglib::psi(alpha_theta + beta_theta) - psi_beta_cached);
+			return sum_dlp_xij_dbeta + cobs()*(alglib::psi(x[0] + x[1]) - psi_beta_cached);
 		}
 
 		// AKA -lp_xi_given_hparams
-		double operator() (const vector<double> &x) const {
+		double operator() (vector<double> x, bool exp_args = true) const {
+			if (exp_args) {
+				exp_elements(x);
+			}
 			// xi_vec and ni_vec are class members
 			// x[0] is alpha_theta, x[1] is beta_theta
 			double sum_lp_xij = 0.0;
@@ -583,8 +608,8 @@ struct bayes_orient_bias_quant_f : public base_orient_bias_quant_f {
 
 		void accumulate (vector<double> &current_grad) {
 			vector<double> new_grad (2);
-			new_grad[0] = dlp_xi_given_hparams_dalpha(curr_params[0], curr_params[1]);
-			new_grad[1] = dlp_xi_given_hparams_dbeta(curr_params[0], curr_params[1]);
+			new_grad[0] = dlp_xi_given_hparams_dalpha(curr_params);
+			new_grad[1] = dlp_xi_given_hparams_dbeta(curr_params);
 			stograd::add_to(new_grad, current_grad);
 			next();
 		}
@@ -600,28 +625,30 @@ struct bayes_orient_bias_quant_f : public base_orient_bias_quant_f {
 		const bayes_orient_bias_quant_f &parent;
 
 		// fixed parameters for objective function (previously estimated)
+		// values are in real space
 		const double alpha_theta;
 		const double beta_theta;
 
 		// vector of parameters being optimized
+		// values are in unconstrained (log) space 
 		// [0] = alpha_phi
 		// [1] = beta_phi
 		vector<double> curr_params;
 
 		double alpha() const {
-			return curr_params[0];
+			return exp(curr_params[0]);
 		}
 
 		double beta() const {
-			return curr_params[1];
+			return exp(curr_params[1]);
 		}
 
 		// index of record (in parent object) currently being analyzed
 		size_t J;
 
-		phi_hparams_optimizable(const bayes_orient_bias_quant_f &P, vector<double> &initial_params,
+		phi_hparams_optimizable(const bayes_orient_bias_quant_f &P, vector<double> initial_params,
 				const double a_theta, const double b_theta) :
-				parent(P), alpha_theta(a_theta), beta_theta(b_theta), curr_params(initial_params), J(0) {}
+				parent(P), alpha_theta(a_theta), beta_theta(b_theta), curr_params(log_c(initial_params)), J(0) {}
 
 		size_t nobs() const {
 			return parent.xi_vec.size();
@@ -665,7 +692,10 @@ struct bayes_orient_bias_quant_f : public base_orient_bias_quant_f {
 		}
 
 		// AKA -lp_xc_given_params 
-		double operator() (const vector<double> &x) const {
+		double operator() (vector<double> x, bool exp_args = true) const {
+			if (exp_args) {
+				exp_elements(x);
+			}
 			double sum_lp_xcj = 0.0;
 			double sum_log_ncp = 0.0;
 			for (size_t j = 0; j < cobs(); ++j) {
