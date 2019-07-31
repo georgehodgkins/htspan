c5cdcf4b3c91b77d3521affd6fa3c2c4d608d9b3
George Hodgkins <hodgkinsgeorge@gmail.com>
2019-07-31 09:54:02 -0500

    Refactored filter system test

--- test/test-orient-bias-filter.sh
+++ test/test-orient-bias-filter.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 
+set -euo pipefail
+
+BIN="../bin"
+SIM="../data/sim"
+
+# individual identification test function
+ident_test () {
+	if [ $# -ge 3 ]; then
+		SRC=$1
+		DTYPE=$2
+		MODEL=$3
+	else
+		echo "Arguments to ident-test: <src=orig|ffpe|oxog> <dtype=ffpe|oxog> <model=bayes|freq> [<args_to_pass>]"
+		exit 1
+	fi
+
+	PASS_ARGS=""
+
+	if [ $# -ge 4 ]; then
+		while [ "$4" != "" ]; do
+			PASS_ARGS="$PASS_ARGS $4"
+			shift
+		done
+	fi
+
+	rm -f ${SIM}/out/${SRC}.${DTYPE}.${MODEL}.*
+
+	${BIN}/hts-orient-bias identify --stdout -t ${DTYPE} -b ${SIM}/sim.${SRC}.bam -V ${SIM}/sim.${SRC}.calls.snv -o out/${SRC}.${DTYPE}.${MODEL}.snv --log-file out/${SRC}.${DTYPE}.${MODEL}.out -M ${MODEL} ${PASS_ARGS}
+
+	if [ -e out/${SRC}.${DTYPE}.${MODEL}.snv ]; then
+		echo -n $(diff ${SIM}/sim.orig.snv <(cut -f -4 out/${SRC}.${DTYPE}.${MODEL}.snv) | grep "^>" | wc -l) #false positives
+		echo -n " "
+		echo $(diff ${SIM}/sim.orig.snv <(cut -f -4 out/${SRC}.${DTYPE}.${MODEL}.snv) | grep "^<" | wc -l) #false negatives
+		exit 0
+	else
+		exit 1
+	fi
+}
+
 DTYPES=("oxog" "ffpe")
 TRUE_PHI=".2"
 REF_FILE="../data/tp53_hg38.fasta"
